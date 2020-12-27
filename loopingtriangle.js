708abd8e8d8d311f6b98cdf7ac379197c26c9107
GBM <72363278+gbmgithub@users.noreply.github.com>
2020-12-27 16:36:28 -0300

    Add files via upload

---
+++ loopingtriangle.js
@@ -0,0 +1,16 @@
+//for (let count = 0; count = 7; count +=1);
+    //console.log('#');
+    //console.log(' ');
+//let count = 0;
+//while (count != 7)
+    //console.log('#'*count);
+    //count = count + 1;
+
+
+
+
+let number = 0;
+while (number <= 12) {
+    console.log(number*'#');
+    number = number + 2;
+}
