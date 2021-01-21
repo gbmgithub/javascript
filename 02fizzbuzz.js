1832c89d45c67f909caa27ea412e89c351861732
GBM <72363278+gbmgithub@users.noreply.github.com>
2021-01-21 15:33:35 -0300

    Update 02fizzbuzz.js

--- 02fizzbuzz.js
+++ 02fizzbuzz.js
@@ -4,4 +4,5 @@ for (let num = 1; num <= 100; num++) {
     if (num % 5 == 0) disp += "Buzz";
     else if (num % 3 == 0) if (num % 5 == 0) disp += "FizzBuzz"
     console.log(disp || num);
+    
   }
